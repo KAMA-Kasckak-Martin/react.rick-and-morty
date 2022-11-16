86c4b25a116065978b880b1dabc0f8f8871d8900
martinkascak <m.kascak93@gmail.com>
2022-11-16 17:13:50 +0100

    add Cardcmponent

--- src/components/GalleryComponent.tsx
+++ src/components/GalleryComponent.tsx
@@ -1,8 +1,15 @@
 
 import React from "react";
+import {Charekter} from "../model/Charekter";
+import {CHARACTER} from "./characterRickAndMorty";
+
+type GalleryComponentProps={
+    characters:Charekter[]
+}
 
 
-export default function GalleryComponent(){
-
-    return
+export default function GalleryComponent(props:GalleryComponentProps){
+    const charakterComponents = props.characters.map((charakter,index) => {
+      return<
+    })
 }
---
+++ src/model/Charekter.ts
@@ -0,0 +1,6 @@
+export type Charekter={
+    name:string,
+    image:string,
+    status:string,
+    id:number
+}
